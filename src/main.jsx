cfc41b551aa7991fe099ed1817b18c143632f0b6
bradyt88 <bradythomas0936@gmail.com>
2026-09-14 22:14:07 +0100

    Wrap DarkMind component in ErrorBoundary

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import DarkMind from "../DarkMind.jsx";
+import ErrorBoundary from "./ErrorBoundary.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode><DarkMind /></React.StrictMode>
+  <React.StrictMode>
+    <ErrorBoundary>
+      <DarkMind />
+    </ErrorBoundary>
+  </React.StrictMode>
 );
