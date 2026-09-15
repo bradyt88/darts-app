cf123d45606b246b89c132272d0067acf4d695b7
bradyt88 <bradythomas0936@gmail.com>
2026-09-15 17:13:40 +0100

    Restore app render while fixing hero artwork

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import ErrorBoundary from "./ErrorBoundary.jsx";
 import "./darkmind-visuals.css";
 import "./darkmind-mark.css";
 import "./dartzone-home.css";
-import "./dartzone-hero-image.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
