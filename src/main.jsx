f8fcd323a373e6f9ca993d8ac828553de735e623
bradyt88 <bradythomas0936@gmail.com>
2026-09-15 15:06:30 +0100

    Apply Dark Mind visual theme

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import DarkMind from "../DarkMind.jsx";
 import ErrorBoundary from "./ErrorBoundary.jsx";
+import "./darkmind-visuals.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
